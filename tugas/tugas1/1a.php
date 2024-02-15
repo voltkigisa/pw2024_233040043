083a1d2f1a3db239664fd71798baf1150d6e665c
voltkigisa <raden.233040043@mail.unpas.ac.id>
2024-02-15 09:00:48 +0700

    tugas1a

---
+++ tugas/tugas1/1a.php
@@ -0,0 +1,15 @@
+<?php 
+$topi = "Topi";
+$bundar = "Bundar";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Topi saya bundar</title>
+</head>
+<body>
+    <h2><?php echo '"'.$topi.' saya '.$bundar.', '.$bundar.' '.$topi.' saya."'; ?></h2>
+</body>
+</html>
