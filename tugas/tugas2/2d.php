f0f52e5cf9b0d761b817cf510285f776157930bb
voltkigisa <raden.233040043@mail.unpas.ac.id>
2024-02-23 14:51:53 +0700

    tugas2

--- tugas/tugas2/2d.php
+++ tugas/tugas2/2d.php
@@ -1,47 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>papan catur</title>
-    <style>
-        *{
-            margin: 0;
-            padding: 0;
-        }
-
-       
-        .kotakPutih {
-            width: 50px;
-            height: 50px;
-            display: inline-block;
-            background-color: white;
-        }
-
-        .kotakHitam {
-            width: 50px;
-            height: 50px;
-            display: inline-block;
-            background-color: black;
-        }
-    </style>
-
+    <title>Document</title>
 </head>
-
 <body>
-    
-    <?php
-    for ($baris = 1; $baris <= 5; $baris++) {
-        echo "<br>";
-        for ($kolom = 1; $kolom <= 5; $kolom++) {
-            if (($kolom + $baris) % 2 == 1) {
-                echo '<div class=kotakPutih></div>';
-            } else {
-                echo '<div class=kotakHitam></div>';
+    <table  width="250px"  cellspacing="0px" cellpadding="0px">
+        <?php
+        for ($baris = 1; $baris <= 5; $baris++) {
+            echo "<tr>";
+            for ($kolom = 1; $kolom <= 5; $kolom++) {
+                if (($kolom + $baris) % 2 == 0) {
+                    echo "<td height='50px' width='50px' style='background:#000; border:1px black solid;'></td>";
+                } else {
+                    echo "<td height='50px' width='50px' style='background:#fff; border:1px black solid;' ></td>";
+                }
             }
+            echo "</tr>";
         }
-    }
-    ?>
+        ?>
+    </table>
 </body>
 </html>
+
+
